1fbda86ffc276626350183efa70607aba3c70967
simadivdar <simadivdar@gmail.com>
2023-08-30 18:11:33 +0330

    feat! : simplify the structure

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
+import Head from "../src/components/Header.vue";
+import { useRoute } from "vue-router";
+import { reactive } from "vue";
+const route=useRoute();
+var border = reactive({});
+function borders(event) {
+ console.log(event.value);
+ border.value=event.value;
+ console.log(border.value);
+
+}
 </script>
 
 <template>
-    <router-view></router-view>
+  <div :class="mode" >
+    <Head />
+    <router-view :key="route.path"  :border="border"  @borders="borders"></router-view>
+  </div>
 </template>
 
 <style scoped>
-
 </style>
--- src/router.js
+++ src/router.js
@@ -1,25 +1,17 @@
 import { createRouter, createWebHistory } from "vue-router";
-import ReadParent from "./components/readCountries.vue";
 import Read from "./components/readApiCountries.vue";
 import Show from "./components/showCoutnry.vue";
 const routes = [
   {
-    path: "/",
-    name: "rest-countries",
-    component: ReadParent,
-    children: [
-        {
-            path:"",
-            name: "read-api",
-            component: Read,
-        },
+    path:"",
+    name: "read-api",
+    component: Read,
+  },
       {
         path: "/:country",
         name: "show-country",
         component: Show,
       },
-    ],
-  },
 ];
 const router = createRouter({
   history: createWebHistory(),
